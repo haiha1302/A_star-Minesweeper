01f69c9740fc4f44e402825b91406316af952bce
Nguyen Hai Ha <td12a11haiha@gmail.com>
2021-08-25 21:26:14 +0700

    update 2

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-function init(player, OPPONENT){
+function init(player, OPPONENT) {
     const canvas = document.getElementById("cvs");
     const ctx = canvas.getContext("2d");
 
@@ -37,12 +37,12 @@ function init(player, OPPONENT){
     let GAME_OVER = false;
     
     // Tạo bảng
-    function drawBoard(){
+    function drawBoard() {
         let id = 0
-        for(let i = 0; i < ROW; i++){
+        for(let i = 0; i < ROW; i++) {
             board[i] = [];
-            for(let j = 0; j < COLUMN; j++){
-                board[i][j] = id;
+            for(let j = 0; j < COLUMN; j++) {
+                board[i][j] = id; 
                 id++;
                 // Vẽ bảng
                 ctx.strokeStyle = "#000";
@@ -52,7 +52,7 @@ function init(player, OPPONENT){
     }
     drawBoard();
 
-    canvas.addEventListener("click", function(event){
+    canvas.addEventListener("click", function(event) {
         if(GAME_OVER) return;
         // lấy tọa độ khi click
         let X = event.clientX - canvas.getBoundingClientRect().x;
@@ -73,20 +73,20 @@ function init(player, OPPONENT){
         drawOnBoard(currentPlayer, i, j);
 
         // Kiểm tra game win
-        if(isWinner(gameData, currentPlayer)){
+        if(isWinner(gameData, currentPlayer)) {
             showGameOver(currentPlayer);
             GAME_OVER = true;
             return;
         }
 
         // Kiểm tra hòa
-        if(isTie(gameData)){
+        if(isTie(gameData)) {
             showGameOver("tie");
             GAME_OVER = true;
             return;
         }
 
-        if( OPPONENT == "computer"){
+        if( OPPONENT == "computer") {
             // Lấy id sử dụng thuật toán minimax
             let id = minimax( gameData, player.computer ).id;
             gameData[id] = player.computer;
@@ -95,26 +95,26 @@ function init(player, OPPONENT){
             let space = getIJ(id);
             drawOnBoard(player.computer, space.i, space.j);
 
-            if(isWinner(gameData, player.computer)){
+            if(isWinner(gameData, player.computer)) {
                 showGameOver(player.computer);
                 GAME_OVER = true;
                 return;
             }
-            if(isTie(gameData)){
+            if(isTie(gameData)) {
                 showGameOver("tie");
                 GAME_OVER = true;
                 return;
             }
-        }else{
+        } else {
             // Chuyển lượt chơi
             currentPlayer = currentPlayer == player.man ? player.friend : player.man;
         }
     });
 
-    function minimax(gameData, PLAYER){
-        if( isWinner(gameData, player.computer) ) return { evaluation : +10 };
-        if( isWinner(gameData, player.man)      ) return { evaluation : -10 };
-        if( isTie(gameData)                     ) return { evaluation : 0 };
+    function minimax(gameData, PLAYER) {
+        if(isWinner(gameData, player.computer) ) return { evaluation : +10 };
+        if(isWinner(gameData, player.man)      ) return { evaluation : -10 };
+        if(isTie(gameData)                     ) return { evaluation : 0 };
 
         let EMPTY_SPACES = getEmptySpaces(gameData);
 
@@ -124,39 +124,34 @@ function init(player, OPPONENT){
         for( let i = 0; i < EMPTY_SPACES.length; i++) {
             let id = EMPTY_SPACES[i];
 
-            // BACK UP THE SPACE
             let backup = gameData[id];
 
-            // MAKE THE MOVE FOR THE PLAYER
             gameData[id] = PLAYER;
 
             // Lưu lại giá trị đường đi
             let move = {};
             move.id = id;
-            if( PLAYER == player.computer){
+            if( PLAYER == player.computer) {
                 move.evaluation = minimax(gameData, player.man).evaluation;
-            }else{
+            } else {
                 move.evaluation = minimax(gameData, player.computer).evaluation;
             }
 
-            // RESTORE SPACE
             gameData[id] = backup;
             moves.push(move);
         }
         let bestMove;
-        if(PLAYER == player.computer){
-            // Đường tối đa
+        if(PLAYER == player.computer) {
             let bestEvaluation = -Infinity;
-            for(let i = 0; i < moves.length; i++){
+            for(let i = 0; i < moves.length; i++) {
                 if( moves[i].evaluation > bestEvaluation ) {
                     bestEvaluation = moves[i].evaluation;
                     bestMove = moves[i];
                 }
             }
         }else{
-            // Đường tối ưu
             let bestEvaluation = +Infinity;
-            for(let i = 0; i < moves.length; i++){
+            for(let i = 0; i < moves.length; i++) {
                 if( moves[i].evaluation < bestEvaluation ) {
                     bestEvaluation = moves[i].evaluation;
                     bestMove = moves[i];
@@ -166,48 +161,49 @@ function init(player, OPPONENT){
         return bestMove;
     }
 
-    function getEmptySpaces(gameData){
+    function getEmptySpaces(gameData) {
         let EMPTY = [];
-        for( let id = 0; id < gameData.length; id++){
+        for( let id = 0; id < gameData.length; id++) {
             if(!gameData[id]) EMPTY.push(id);
         }
         return EMPTY;
     }
 
-    function getIJ(id){
-        for(let i = 0; i < board.length; i++){
+    function getIJ(id) {
+        for(let i = 0; i < board.length; i++) {
             for(let j = 0; j < board[i].length; j++){
-                if(board[i][j] == id) return { i : i, j : j}
+                if(board[i][j] == id)
+                    return { i : i, j : j}
             }
         }
     }
 
-    function isWinner(gameData, player){
+    function isWinner(gameData, player) {
         for(let i = 0; i < COMBOS.length; i++){
             let won = true;
-            for(let j = 0; j < COMBOS[i].length; j++){
+            for(let j = 0; j < COMBOS[i].length; j++) {
                 let id = COMBOS[i][j];
                 won = gameData[id] == player && won;
             }
-            if(won){
+            if(won) {
                 return true;
             }
         }
         return false;
     }
 
-    function isTie(gameData){
+    function isTie(gameData) {
         let isBoardFill = true;
-        for(let i = 0; i < gameData.length; i++){
+        for(let i = 0; i < gameData.length; i++) {
             isBoardFill = gameData[i] && isBoardFill;
         }
-        if(isBoardFill){
+        if(isBoardFill) {
             return true;
         }
         return false;
     }
 
-    function showGameOver(player){
+    function showGameOver(player) {
         let message = player == "tie" ? "Oops No Winner" : "The Winner is";
         let imgSrc = `img/${player}.png`;
         gameOverElement.innerHTML = `
@@ -218,10 +214,8 @@ function init(player, OPPONENT){
         gameOverElement.classList.remove("hide");
     }
 
-    function drawOnBoard(player, i, j){
+    function drawOnBoard(player, i, j) {
         let img = player == "X" ? xImage : oImage;
-
-        // the x,y positon of the image are the x,y of the clicked space
         ctx.drawImage(img, j * SPACE_SIZE, i * SPACE_SIZE);
     }
 }
